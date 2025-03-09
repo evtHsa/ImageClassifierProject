f6b841f1ca59c14e3a5dbad735989ab7531e65d3
eric van tassell <Eric.VanTassell@amd.com>
2025-03-09 14:41:36 -0500

    whatever

--- util.py
+++ util.py
@@ -65,7 +65,7 @@ def get_classifier():
     return classifier
     
 def get_model(args):
-    mod_name = args.model
+    mod_name = args.model[0]
     ctor = getattr(models, mod_name)
     model = ctor(pretrained=True)
     model.arch = mod_name
@@ -82,7 +82,7 @@ def load_chkpt(args, model):
     else: #map_location=torch.device("cpu"
         checkpoint = torch.load(args.chkpt_pth, map_location=torch.device("cpu"))
         
-    assert(checkpoint['arch'] == args.model)
+    assert(checkpoint['arch'] == args.model[0])
     
         
     model.arch         = checkpoint['arch'] 
